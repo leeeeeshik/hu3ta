5174c874d76e450c3fbb012e3fa8a416884c4b4b
Tyuiu.SheludkovAA@outlook.com <Tyuiu.SheludkovAA@outlook.com>
2023-11-24 11:16:29 +0500

    biblioteka

--- opr3/matrix3.cs
+++ opr3/matrix3.cs
@@ -10,6 +10,7 @@ namespace opr3
     {
         public int opred3por(int[,] ar)
         {
+
             int res = ar[0, 0] * ar[1, 1] * ar[2, 2] + ar[0, 1] * ar[1, 2] * ar[2, 0] + ar[1, 0] * ar[2, 1] * ar[0, 2] - ar[0, 2] * ar[1, 1] * ar[2, 0] - ar[0, 1] * ar[1, 0] * ar[2, 2] - ar[0, 0] * ar[1, 2] * ar[2, 1];
             return res;
         }
