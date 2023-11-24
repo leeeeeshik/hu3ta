c2b6100c029763d3518421a17c0ce60c235ec677
Tyuiu.SheludkovAA@outlook.com <Tyuiu.SheludkovAA@outlook.com>
2023-11-24 11:15:43 +0500

    Add project files.

---
+++ opr3/matrix3.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace opr3
+{
+    public class matrix3
+    {
+        public int opred3por(int[,] ar)
+        {
+            int res = ar[0, 0] * ar[1, 1] * ar[2, 2] + ar[0, 1] * ar[1, 2] * ar[2, 0] + ar[1, 0] * ar[2, 1] * ar[0, 2] - ar[0, 2] * ar[1, 1] * ar[2, 0] - ar[0, 1] * ar[1, 0] * ar[2, 2] - ar[0, 0] * ar[1, 2] * ar[2, 1];
+            return res;
+        }
+    }
+}
---
+++ opr3por/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using opr3;
+
+namespace opr3por
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            matrix3 fnc = new matrix3();
+            int[,] mtr = new int[3,3];
+            Console.WriteLine("Введите элементы матрицы :");
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    
+                    mtr[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine("Матрица");
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write($"{mtr[i, j]}\t");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+            Console.WriteLine("Определитель этой матрицы равен :");
+            Console.WriteLine(fnc.opred3por(mtr));
+            Console.ReadKey();
+
+        }
+    }
+}
